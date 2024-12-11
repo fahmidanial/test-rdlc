7f7d6d7f8b5fe061e534615bc81d4f9b17075899
fahmidanial <fahmidanial9091@gmail.com>
2024-12-12 00:57:48 +0800

    update static file

---
+++ test-rdlc/wwwroot/js/app.js
@@ -0,0 +1,10 @@
+﻿window.downloadFileFromStream = async (fileName, contentType, content) => {
+    const blob = new Blob([content], { type: contentType });
+    const url = window.URL.createObjectURL(blob);
+    const anchorElement = document.createElement('a');
+    anchorElement.href = url;
+    anchorElement.download = fileName;
+    anchorElement.click();
+    anchorElement.remove();
+    window.URL.revokeObjectURL(url);
+}
