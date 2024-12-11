0aa63ef3192bb54e83c29bd4c07c11b6c53894be
fahmidanial <fahmidanial9091@gmail.com>
2024-12-12 01:38:54 +0800

    update

--- test-rdlc/Program.cs
+++ test-rdlc/Program.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using test_rdlc;
-using test_rdlc.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<ReportService>();  // Register the service
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7147/") }); // Update with your API URL
 
 await builder.Build().RunAsync();
