ef2cddc83306d6ac8ad7d925cfc5bdd08dd24c67
fahmidanial <fahmidanial9091@gmail.com>
2024-12-12 00:58:14 +0800

    add services and dummy models

---
+++ test-rdlc/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace test_rdlc.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductNumber { get; set; }
+    }
+}
---
+++ test-rdlc/Services/ReportService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Reporting.NETCore;
+using RDLC_Demo.Client.Models;
+using test_rdlc.Models;
+
+namespace RDLC_Demo.Client.Services
+{
+    public class ReportService
+    {
+        public byte[] GenerateReport(string reportType)
+        {
+            var items = GetDummyData();
+            var parameters = new[] { new ReportParameter("Title", "Product Report") };
+
+            var report = new LocalReport();
+            var assembly = typeof(Product).Assembly;
+
+            using (var rs = assembly.GetManifestResourceStream("RDLC_Demo.Client.Reports.Report1.rdlc"))
+            {
+                report.LoadReportDefinition(rs);
+                report.DataSources.Add(new ReportDataSource("DataSet1", items));
+                report.SetParameters(parameters);
+
+                return report.Render(reportType);
+            }
+        }
+
+        private Product[] GetDummyData()
+        {
+            return new[] {
+                new Product { ProductID = 1, ProductName = "Adjustable Race", ProductNumber = "AR-5381" },
+                new Product { ProductID = 2, ProductName = "Bearing Ball", ProductNumber = "BA-8327" }
+            };
+        }
+    }
+}
